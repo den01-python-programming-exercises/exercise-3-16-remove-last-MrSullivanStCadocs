78f621137a17fd5291469b3c1e82ec9ef532ba09
MrSullivan <>
2021-05-30 10:53:22 +0000

    two methods, remove last stings command, pop command with -1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,25 @@
 def main():
     #write your code below this line
+    strings = []
+
+    strings.append("First")
+    strings.append("Second")
+    strings.append("Third")
+
+    print(strings)
+
+    remove_last(strings)
+    remove_last(strings)
+
+    print(strings)
+
+def remove_last(strings):
+
+    if len(strings) == 0:
+      return
+    strings.pop(-1)
+
+    
 
 if __name__ == '__main__':
     main()
